681f6f9066897c6a598d6c91efd5b49e99ac86e1
mayar <mayarhamed15@gmail.com>
2023-03-24 21:36:48 +0200

    missing job schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,15 +4,14 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\PruneOldPostsJob;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     */
+
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new PruneOldPostsJob)->daily();
     }
 
     /**
